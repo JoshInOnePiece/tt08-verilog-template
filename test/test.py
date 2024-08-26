06d989cddc4d21e0ed5ad342eed3ccbaa0c8a5a5
Joshua Joseph <jo634076@ucf.edu>
2024-08-26 01:21:05 -0400

    changed all the clk variables to iClk

--- test/test.py
+++ test/test.py
@@ -23,58 +23,58 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.iRst.value = 0
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.iClk, 1)
     
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iLoad_key.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iLoad_key.value = 0
 
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
 
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iLoad_msg.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 0
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iData_in.value = 1
-    await ClockCycles(dut.clk, 4)
+    await ClockCycles(dut.iClk, 4)
     dut.iLoad_msg.value = 0
 
-    await ClockCycles(dut.clk, 80)
+    await ClockCycles(dut.iClk, 80)
     """""
     for i in range(256):
         dut.uio_in.value = i
-        await ClockCycles(dut.clk, 1)
+        await ClockCycles(dut.iClk, 1)
         assert dut.uo_out.value == i
 
     # When under reset: Output is uio_in, uio is in input mode
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.iClk, 1)
     assert dut.uio_oe.value == 0
     for i in range(256):
         dut.ui_in.value = i
-        await ClockCycles(dut.clk, 1)
+        await ClockCycles(dut.iClk, 1)
         assert dut.uo_out.value == i
     """""
